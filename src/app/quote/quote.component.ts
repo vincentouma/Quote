024c48e9eec222deb7c346aa30deb28edef6418b
vincentouma <vinceoumah@gmail.com>
2019-07-07 19:20:05 +0300

    Add date count pipe

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son' new Date(2019,7,10),
-    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot' new Date(2019,7,10),
-    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon' new Date(2019,7,10),
-    new Quote(4,'Get Dog Food','Pupper likes expensive snacks' new Date(2019,7,10),
-    new Quote(5,'Solve math homework','Damn Math' new Date(2019,7,10),
-    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord' new Date(2019,7,10),
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', new Date(2019,7,10)),
+    new Quote(2,'Buy Cookies','I have to buy cookies for the parrot', new Date(2019,7,11)),
+    new Quote(3,'Get new Phone Case','Diana has her birthday coming up soon', new Date(2019,7,12)),
+    new Quote(4,'Get Dog Food','Pupper likes expensive snacks', new Date(2019,7,13)),
+    new Quote(5,'Solve math homework','Damn Math', new Date(2019,7,14)),
+    new Quote(6,'Plot my world domination plan','Cause I am an evil overlord', new Date(2019,7,15)),
     ];
 
     toggleDetails(index){
